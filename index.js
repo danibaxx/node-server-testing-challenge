e3a93cbef629a1809c65c87a605553725158d5f4
Danielle B <danibaxx@hotmail.com>
2020-01-23 17:47:30 -0700

    db-config.js made, index.js scaffolded

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const db = require('./data/db-config');
+
+const server = express();
+const port = process.env.PORT || 4000;
+
+server.use(express.json());
+
+server.get((req, res, next) =>{ 
+
+});
+
+server.get((req, res, next) => {
+
+});
+
+server.post((req, res, next) => {
+
+});
+
+server.delete((req, res, next) => {
+
+});
