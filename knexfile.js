b43e2d26f7de81ac8a0909ed69bcb7d3fa2bb894
Danielle B <danibaxx@hotmail.com>
2020-01-23 17:30:46 -0700

    project init, with downloaded needed dependencies, create knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,31 @@
+const sqlite = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./data/migrations",
+  },
+  seeds: {
+    directory: "./data/seeds",
+  },
+  pool : {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done);
+    },
+  },
+};
+
+module.exports = {
+  dev: {
+    ...sqlite,
+    connection: {
+      filename: "./data/zoos-dev.db3",
+    },
+  },
+  test: {
+    ...sqlite,
+    connection: {
+      filename: "./data/zoos-test.db3",
+    },
+  },
+};
+
